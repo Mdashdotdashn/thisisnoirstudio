a8f1a48bc3d120a3ce9bd74c8690b16b5138a1a7
mre <mre@ableton.com>
2017-10-26 15:17:07 +0200

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,6 +58,56 @@ $('#left-slides').vegas({
         }, 3000);
     });
 
+
+/*----- Loading lissajou ----- BASED ON Misha Heesakkers PEN https://codepen.io/MishaHahaha/pen/ONQQNY */ 
+window.onload = function () {
+  const canvas = document.getElementById("loading-lissa")
+  const context = canvas.getContext('2d')
+  
+  const radius = canvas.width / 6
+  const angleStep = Math.PI * 2 / 360
+  let theta = 0
+    
+  //change frequencies for getting various curves
+  const frequencyX = 7
+  const frequencyY = 7
+  
+  window.requestAnimationFrame(draw)
+  
+  function draw () {
+    context.setTransform(1, 0, 0, 1, 1, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    
+    context.setTransform(1, 0, 0, 1, canvas.width / 2, canvas.height / 2);
+    context.beginPath();
+
+    for (let angle = 0; angle < Math.PI * 5; angle += angleStep ) {
+      const x = Math.sin(angle * frequencyX + theta) * Math.cos(angle + theta) * radius;
+      const y = Math.cos(angle * frequencyY) * Math.sin(angle + theta) * radius;
+      if(angle === 0) {
+        context.moveTo(x, y)
+      } else {
+        context.lineTo(x, y)   
+      }
+    }
+
+
+var gradient=ctx.createLinearGradient(0,0,170,10);
+gradient.addColorStop("0","#22FFB3");
+gradient.addColorStop("1.0","#000000");
+
+    context.lineWidth = 2
+    context.strokeStyle = gradient;
+    context.stroke()
+    context.miterLimit = .5;
+    context.closePath()
+    
+    theta += 0.03
+    window.requestAnimationFrame(draw);
+  }
+
+}
+
 /* ----- Wow init ----- */
 
 
